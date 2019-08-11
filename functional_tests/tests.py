9db8a3d953a50605ec9a4cd2630dc53ffdd0568b
Adam Charane <adamcharane@gmail.com>
2019-08-11 06:40:10 +0100

    adding unit tests, views, and urls for the FT: URL for creating lists via POST

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
+MAX_WAIT = 0.5
 
 class NewVisitorTest(LiveServerTestCase):
     
